9fa4cf8b3d4db7688ca9192dda9384521fa505e5
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-26 09:02:29 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    if (school == true) {
+        basic.showString("go to school")
+    } else {
+        basic.showString("stay home")
+    }
 })
+let school = false
+school = true
